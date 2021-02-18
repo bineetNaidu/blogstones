321c80a17f6c37b94250803e53b91377f3658acd
bineetNaidu <bineet.s.naidu@gmail.com>
2021-02-18 13:36:36 +0530

    🚀 add Header

---
+++ client/Components/Header.jsx
@@ -0,0 +1,33 @@
+import Link from 'next/link';
+
+const Header = () => {
+  return (
+    <header className="flex py-5 mx-12">
+      <div className="flex-1">
+        <Link href="/">
+          <h1 className="text-2xl cursor-pointer">BlogStones.dev</h1>
+        </Link>
+      </div>
+
+      <div>
+        <Link href="/signup">
+          <span className="text-green-500 hover:text-white cursor-pointer text-lg transition mx-1">
+            Signup
+          </span>
+        </Link>
+        <Link href="/login">
+          <span className="text-green-500 hover:text-white cursor-pointer text-lg transition mx-1">
+            Login
+          </span>
+        </Link>
+        <Link href="/create">
+          <span className="text-green-500 hover:text-white cursor-pointer text-lg transition mx-1">
+            Create a Blog
+          </span>
+        </Link>
+      </div>
+    </header>
+  );
+};
+
+export default Header;
--- client/pages/_app.jsx
+++ client/pages/_app.jsx
@@ -1,14 +1,23 @@
 import Head from 'next/head';
 import 'tailwindcss/tailwind.css';
+import Header from '../Components/Header';
 
 function MyApp({ Component, pageProps }) {
+  const gradientCss = {
+    background: '#0f2027',
+    background: '-webkit-linear-gradient(to bottom, #0f2027, #203a43, #2c5364)',
+    background: 'linear-gradient(to bottom, #0f2027, #203a43, #2c5364)',
+  };
   return (
     <>
       <Head>
         <title>BlogStones.dev</title>
         <link rel="icon" href="/favicon.ico" />
       </Head>
-      <Component {...pageProps} />
+      <main className="min-h-screen h-full text-green-200" style={gradientCss}>
+        <Header />
+        <Component {...pageProps} />
+      </main>
     </>
   );
 }
