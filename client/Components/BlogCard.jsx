a9933e4c32b43e436ceca393824a773baca8bae0
bineetNaidu <bineet.s.naidu@gmail.com>
2021-02-18 15:46:48 +0530

    ✨ improve UI UX for BlogCard

--- client/Components/BlogCard.jsx
+++ client/Components/BlogCard.jsx
@@ -14,36 +14,39 @@ const BlogCard = ({ blog }) => {
         className="flex flex-col rounded-lg shadow-lg overflow-hidden transform transition hover:-translate-y-1 hover:shadow-xl"
         key={blog.id}
       >
-        {/* <div>
+        <div>
           <img
             className="h-48 w-full object-cover"
-            src={post.frontmatter.cover_image}
-            alt={post.frontmatter.title}
+            src={blog.thumbnailImg || 'https://bit.ly/3jZzeX0'}
+            alt={blog.name + "'s Thumbnail"}
           />
-        </div> */}
+        </div>
         <div className="bg-green-300 dark:bg-green-900 p-6 flex flex-col">
-          <div className="flex space-x-1 text-sm text-gray-500 dark:text-gray-300">
+          <div className="flex space-x-1 text-sm text-green-900 dark:text-gray-300">
             <time dateTime={blog.createAt}>
               {new Date(blog.createAt).toDateString()}
             </time>
           </div>
           <div className="mt-2">
-            <p className="text-2xl font-bold text-gray-900 dark:text-gray-100">
+            <p className="text-2xl font-extrabold text-green-900 dark:text-gray-100">
               {blog.name}
             </p>
-            <p className="text-lg mt-3 text-gray-600 dark:text-purple-100 flex justify-center">
-              <span className="flex items-baseline mx-1">
-                {blog.likes}
-                <FontAwesomeIcon icon={faThumbsUp} />
-              </span>{' '}
-              <span className="flex items-baseline mx-1">
-                {blog.dislikes}
-                <FontAwesomeIcon icon={faThumbsDown} />
-              </span>{' '}
-              <span className="flex items-baseline mx-1">
-                {blog._commentsMeta.count}
-                <FontAwesomeIcon icon={faComment} />
-              </span>
+            <p className="text-lg mt-3 text-green-900 dark:text-purple-100 flex justify-between items-center">
+              <h1 className="italic">- {blog.author.name}</h1>
+              <div className="flex">
+                <span className="flex items-baseline mx-1">
+                  {blog.likes}
+                  <FontAwesomeIcon icon={faThumbsUp} />
+                </span>{' '}
+                <span className="flex items-baseline mx-1">
+                  {blog.dislikes}
+                  <FontAwesomeIcon icon={faThumbsDown} />
+                </span>{' '}
+                <span className="flex items-baseline mx-1">
+                  {blog._commentsMeta.count}
+                  <FontAwesomeIcon icon={faComment} />
+                </span>
+              </div>
             </p>
           </div>
         </div>
--- client/pages/index.jsx
+++ client/pages/index.jsx
@@ -8,7 +8,7 @@ export default function Home() {
     return <h1 className="text-red-700"> Error:{error.message}</h1>;
   }
   return (
-    <div className="text-center">
+    <div className="text-center px-12">
       <main>
         <h1 className="text-4xl my-6 text-green-400 font-bold underline">
           Latest Blogs
@@ -18,7 +18,7 @@ export default function Home() {
         </h3>
       </main>
       <section className="flex">
-        <article className="mt-12 max-w-lg mx-auto grid gap-5 lg:grid-cols-3 lg:max-w-none">
+        <article className="mt-12 max-w-lg mx-auto grid gap-5 lg:grid-cols-3 lg:max-w-none transition-all">
           {data?.allBlogs?.map((blog) => (
             <BlogCard blog={blog} key={blog?.id} />
           ))}
